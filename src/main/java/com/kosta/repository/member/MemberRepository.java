53c1a14c7c45ccab8d13f2006dcb4d821361b850
dengo25 <dengo25.dev@gmail.com>
2025-06-27 10:53:33 +0900

    [Feat] Member ID 추가 && Member Servie, Repository 추가

--- src/main/java/com/kosta/repository/member/MemberRepository.java
+++ src/main/java/com/kosta/repository/member/MemberRepository.java
@@ -1,4 +1,13 @@
 package com.kosta.repository.member;
 
-public interface MemberRepository {
+import com.kosta.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member , Long> {
+  
+  Member findByMemberName(String memberName);
+  
+  boolean existsByMemberId(String memberId);
 }
---
+++ src/main/java/com/kosta/repository/member/MemberSocialRepository.java
@@ -0,0 +1,12 @@
+package com.kosta.repository.member;
+
+import com.kosta.domain.member.Member;
+import com.kosta.domain.member.MemberSocial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberSocialRepository extends JpaRepository<MemberSocial , Long> {
+
+
+
+
+}

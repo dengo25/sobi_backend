368dc84cce265e9bdc156b8740b95892a8616365
wang_si_eun <alo201011@gmail.com>
2025-07-01 18:41:25 +0900

    [Feat] Member 관련 사항 체크아웃 커밋 처리 (service 는 주석처리)

--- src/main/java/com/kosta/repository/member/MemberRepository.java
+++ src/main/java/com/kosta/repository/member/MemberRepository.java
@@ -1,13 +1,16 @@
 package com.kosta.repository.member;
 
 import com.kosta.domain.member.Member;
+import com.kosta.domain.reivew.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member , Long> {
   
-  Member findByMemberName(String memberName);
-  
+  Member findByMemberName(String memberEmail);
+  Member findByMemberId(String memberId);
   boolean existsByMemberId(String memberId);
 }

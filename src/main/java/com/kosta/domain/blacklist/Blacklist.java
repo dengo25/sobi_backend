29c93507f07db4f0073d4d5275ed070fdd0bf55b
wanbinchoi <cwb6278@gmail.com>
2025-07-04 16:12:22 +0900

    feat 관리자 Entity 추가

---
+++ src/main/java/com/kosta/domain/blacklist/Blacklist.java
@@ -0,0 +1,43 @@
+package com.kosta.domain.blacklist;
+
+import java.time.LocalDateTime;
+
+import com.kosta.domain.member.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "BLACK_LIST")
+@Data
+public class Blacklist {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int blackListNo;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "MEMBER_ID", referencedColumnName = "MEMBER_ID")
+	private Member member;
+	
+	@Column(name = "UPDATE_AT")
+	private LocalDateTime updateAt;
+	
+	@Column(name = "STATUS", length = 20)
+	private String status = "BLOCKED";
+	
+	@PrePersist
+	public void setUpdateAt() {
+		if(this.updateAt == null) {
+			this.updateAt = LocalDateTime.now();
+		}
+	}
+}

1a50fc99b700a4ae08c5918efed1e0a1838b6615
dengo25 <dengo25.dev@gmail.com>
2025-07-12 10:55:36 +0900

    [Feat] Cors설정 추가

--- src/main/java/com/kosta/config/WebSecurityConfig.java
+++ src/main/java/com/kosta/config/WebSecurityConfig.java
@@ -114,7 +114,9 @@ public class WebSecurityConfig {
     configuration.setAllowCredentials(true); //자격증명 포함 허용(ex: 쿠키, Authorication 헤더)
     configuration.setAllowedOrigins(List.of(
         "http://localhost:5173",
-        "http://sobi-front.s3-website.ap-northeast-2.amazonaws.com")); //허용할 프론트엔드 도메인
+        "http://sobi-front.s3-website.ap-northeast-2.amazonaws.com",
+        "https://final.thekosta.com",
+        "http://final.thekosta.com")); //허용할 프론트엔드 도메인
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")); //허용 메서드
     configuration.setAllowedHeaders(List.of("*")); //모든 요청 헤더 허용
     configuration.setExposedHeaders(List.of("*")); //응답 헤더 노출
